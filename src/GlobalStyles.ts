be86a1867676ce88e9b4157840637342d3d1488c
Erik <eriksoederlund@hotmail.com>
2022-02-15 15:39:02 +0100

    Added styles for scrollbar on firefox

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -8,10 +8,31 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
 }
 
+html{
+  scrollbar-width: thin;
+  scrollbar-color: #9bcd6a whitesmoke;
+}
+
 body{
   font-family: 'Lexend Deca', sans-serif;
   font-size: 16px;
   background-color: whitesmoke;
+  scrollbar-color: #9bcd6a;
+  
+  &::-webkit-scrollbar {
+    width: 0.4em;
+  }
+  
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #9bcd6a;
+    outline: 1px solid slategrey;
+    border-radius: 4px;
+  }
 }
 
 
